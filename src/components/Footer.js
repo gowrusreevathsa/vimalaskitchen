771b381a8f7e520dfe1b72075db03d1151164546
Sreevathsa <sreevathsagowru@gmail.com>
2021-01-06 20:44:22 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button } from "./Button";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -19,6 +21,71 @@ function Footer() {
             />
             <Button buttonStyle="btn--outline">Subscribe</Button>
           </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              Vimala's Kitchen <i className="fas fa-mitten left-padding-5" />
+            </Link>
+          </div>
+          <small className="website-rights">Vimala's Kitchen © 2020</small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f"></i>
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram"></i>
+            </Link>
+            <Link
+              className="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i className="fab fa-youtube"></i>
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i className="fab fa-twitter"></i>
+            </Link>
+          </div>
         </div>
       </section>
     </div>
